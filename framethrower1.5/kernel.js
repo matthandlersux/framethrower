746dbffcd1e112ed9e0c88f71e5ae1e08ea88acb
echostorm <webmaster@eversplosion.com>
2008-05-12 21:14:38 +0000

    Initial import.

---
+++ framethrower1.5/kernel.js
@@ -0,0 +1,114 @@
+
+// localIds.get() returns a unique Id
+var localIds = function () {
+	var count = 0;
+	return {
+		get: function () {
+			count++;
+			return "local" + count;
+		}
+	}
+}();
+
+function genString(from, type, to) {
+	var ret = "";
+	if (from) {
+		ret += from.getId();
+	}
+	ret += ",";
+	if (type) {
+		ret += type.getId();
+	}
+	ret += ",";
+	if (to) {
+		ret += to.getId();
+	}
+	return ret;
+}
+
+function genAllStrings(from, type, to) {
+	var ret = [];
+	ret.push(genString(from, type, to));
+	ret.push(genString(null, type, to));
+	ret.push(genString(from, null, to));
+	ret.push(genString(from, type, null));
+	ret.push(genString(null, null, to));
+	ret.push(genString(null, type, null));
+	ret.push(genString(from, null, null));
+	return ret;
+}
+
+
+
+function makeProcess() {
+	var hash = {};
+	var id = localIds.get();
+	var content = null;
+	var informsContent = [];
+
+	function inform(transform, links) {
+		
+	}
+	function informContent(transform) {
+		
+	}
+	
+	return {
+		getId: function () {
+			return id;
+		},
+		register: function (link) {
+			var allStrings = genAllStrings(link.getFrom(), link.getType(), link.getTo());
+			forEach(allStrings, function (key) {
+				if (!hash[key]) {
+					hash[key] = {links: {}, informs: {}};
+				}
+				hash[key].links[link.getId()] = link;
+				forEach(hash[key].informs, function (i) {
+					inform(i, hash[key].links);
+				});
+			});
+		},
+		request: function (transform, query) {
+			// assuming query is one of those strings...
+			if (!hash[query]) {
+				hash[query] = {links: {}, informs: {}};
+			}
+			hash[query].informs[transform.getId()] = transform;
+
+			// do the inform immediately, even though hasn't actually been a change...
+			inform(transform, hash[query].links);
+		},
+		requestContent: function (transform) {
+			
+		}
+	}
+}
+
+function makeLink(from, type, to) {
+	var link = makeProcess();
+	link.getFrom = function () {
+		return from;
+	};
+	link.getType = function () {
+		return type;
+	};
+	link.getTo = function () {
+		return to;
+	};
+	// overrides process's getContent..
+	link.getContent = function () {
+		// return XML..
+	}
+}
+
+function makeTransform() {
+	var transform = makeLink();
+	// ...
+}
+
+
+
+var p = makeProcess();
+var q = makeProcess();
+var r = makeProcess();
