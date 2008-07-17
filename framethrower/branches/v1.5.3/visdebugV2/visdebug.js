406ff3d3fd02711ffa0f12ef9f6163fd3bd8ce9d
echostorm <webmaster@eversplosion.com>
2008-07-17 17:09:04 +0000

    test of new subversion location

--- framethrower/branches/v1.5.3/visdebugV2/visdebug.js
+++ framethrower/branches/v1.5.3/visdebugV2/visdebug.js
@@ -1,6 +1,6 @@
 /**
- * @author boobideedoobop
- */
+* @author boobideedoobop
+*/
 
 
 var screenObjects = {};
@@ -13,18 +13,19 @@ var initUserInput = function() {
 	ui.ym = 0;
 	ui.rPressed = false;
 	ui.zPressed = false;
+	ui.lPressed = false;
 	ui.basezoom=1;
 	ui.zoomfactor=1;
 	ui.selectOid = null;
 	ui.drawnOid = ui.selectOid;
-	
+
 	//setup some display variables
 	ui.svgelements = document.getElementById('svgelements');
 	ui.svgPoint = ui.svgelements.createSVGPoint();
-				
+
 	ui.actual_width = ui.svgelements.width;
 	ui.actual_height = ui.svgelements.height;
-	
+
 	var vb = ui.svgelements.getAttribute("viewBox");
 	if(vb) {
 		var vba = vb.split(" "); //comes out with four string array
@@ -35,28 +36,28 @@ var initUserInput = function() {
 		ui.midx = ui.curorig_x+(ui.orig_width/2);
 		ui.midy = ui.curorig_y+(ui.orig_height/2);
 	}
-	
+
 	ui.calcCoord = function(x,y,ctmNode) {
 		ui.svgPoint.x = x;
 		ui.svgPoint.y = y;
 		var matrix = ctmNode.getScreenCTM();
 		ui.svgPoint = ui.svgPoint.matrixTransform(matrix.inverse());
-	
+
 		//undo the effect of viewBox and zoomin/scroll
 		return ui.svgPoint;
 	};
-	
-	
-	
-	
+
+
+
+
 	dojo.query('#svgelements').connect('onmousemove', function(e) {
-			ui.xm = e.layerX;
-			ui.ym = e.layerY;
+		ui.xm = e.layerX;
+		ui.ym = e.layerY;
 	});
 
 	/*
 	dojo.connect(document, 'onmousedown', function(e) {
-	    if (e.target.localName!=="input") {
+		if (e.target.localName!=="input") {
 			e.preventDefault();
 		}
 	});
@@ -75,18 +76,22 @@ var initUserInput = function() {
 				ui.inity = ui.ym;
 				ui.zPressed = true;
 				ui.basezoom = ui.zoomfactor;
+			} else if(e.keyCode === 76){
+				ui.lPressed = true;
 			}
 		}
 	});
-	
+
 	dojo.connect(document, 'onkeyup', function(e) {
 		if(e.keyCode === 82){
 			ui.rPressed = false;
 		} else if(e.keyCode === 90){
 			ui.zPressed = false;
+		} else if(e.keyCode === 76){
+			ui.lPressed = false;
 		}
 	});
-	
+
 	dojo.connect(document, 'mousedown', function(e) {
 		var targ = e.target;
 		if(targ.id){
@@ -100,8 +105,8 @@ var initUserInput = function() {
 			ui.inity = point.y;
 		}
 	});
-	
-	
+
+
 	return ui;
 };
 
@@ -109,7 +114,6 @@ var initUserInput = function() {
 
 var visDebug = function(){
 	var O = {}; //objects being displayed on screen
-	var L = {}; //links (being displayed if both ends are on screen)
 	var object2svg = compileXSL(loadXMLNow("object2svg.xsl"));
 	var object2html = compileXSL(loadXMLNow("object2html.xsl"));
 	var objectCache = {};
@@ -119,9 +123,9 @@ var visDebug = function(){
 	var initContext = null;
 
 	var ui;
-	
 
-	
+
+
 	//function to help the xml format of the object cache resolve pointers to other objects in the object cache
 	function testTopLevelObject(obj){
 		if (obj.getId) {
@@ -131,43 +135,62 @@ var visDebug = function(){
 		}
 		return false;
 	}
-	
+
 	//function to convert object cache to xml format
 	var objectToXML = makeObjectToXML(testTopLevelObject);
-	
-	
-	
+
+
+
 	var makeScreenObject = function(id){
 		var SO = {};
-		if(!L[id]){
-			L[id] = {};
-			L[id].tolinks = {};			
-		}
-		
+
 		SO.objId = id;
-		
+		SO.links = {};
+		SO.tolinks = {};
+		SO.objectsvg = {};
+		SO.containedObjects = {};
+		SO.angle = 0;
+
+		SO.setX = function(inX){
+			SO.x = inX;
+			SO.targetX = inX;
+		};
+
+		SO.setY = function(inY){
+			SO.y = inY;
+			SO.targetY = inY;
+		};
+
+		SO.setR = function(inR){
+			SO.r = inR;
+			SO.targetR = inR;
+		};
+
 		//give object an x and y
-		SO.x = Math.random()*400;
+		SO.setX(Math.random()*400);
 		SO.prevX = SO.x;
-		SO.y = Math.random()*400;
+		SO.setY(Math.random()*400);
 		SO.prevY = SO.y;
-		SO.r = 300 + Math.random()*8;
+		SO.setR(300 + Math.random()*8);
 		SO.z = 0;
 
-		
 		SO.removeObject = function(){
 			//remove from-links from "to" parameter of links
-			for (var key in L[id].links) {
-				if(L[id].links.hasOwnProperty(key)){
-					var toindex = L[id].links[key].xmlNode.getAttribute('to');
-					if(L[toindex].tolinks[key]){
-						L[toindex].tolinks[key] = undefined;						
+			for (var key in SO.links) {
+				if(SO.links.hasOwnProperty(key)){
+					var toindex = SO.links[key].xmlNode.getAttribute('to');
+					if(O[toindex].tolinks[key]){
+						O[toindex].unregisterToLink(key, SO);
+						SO.unregisterFromLink(key, O[toindex]);
+						//O[toindex].tolinks[key] = undefined;
+						delete O[toindex].tolinks[key];
 					}
 				}
 			}
-			
+
 			var svgdiv = document.getElementById('svgelements');
 			//remove from-links and to-links svg
+			console.log('removing: ' + id);
 			var removeLinks = function(links){
 				forEach(links, function(link){
 					if(link.svg && link.svg.node && svgdiv === link.svg.node.parentNode){
@@ -175,38 +198,36 @@ var visDebug = function(){
 					}
 				});
 			};
-			removeLinks(L[id].links);
-			removeLinks(L[id].tolinks);
-			
+			removeLinks(SO.links);
+			removeLinks(SO.tolinks);
+
 			//clear from-links svg
-			L[id].links = undefined;
-			
+			//SO.links = undefined;
+			//delete SO.links; not necessary?
+
 			//remove object svg
 			if(svgdiv === SO.objectsvg.parentNode){
 				svgdiv.removeChild(SO.objectsvg);
 			}
 			//clear object svg
-			SO.objectsvg = undefined;
+			//SO.objectsvg = undefined;
+			delete SO.objectsvg;
 		};
-		
+
 		SO.setObject = function(obj){
-			if(L[id].links){
-				SO.removeObject();
-			}
-			
 			var xmlresult = objectToXML(obj, obj.getType(), "link");
 			SO.xmlNodes = {};
 			SO.xmlNodes.obj = xmlresult.obj;
 
-			L[id].links = xmlresult.links;
-			
+			SO.links = xmlresult.links;
+
 			//create svg for the objects			
 			var svgresult = object2svg(SO.xmlNodes.obj, {fromx:'0',fromy:'0',r:'20',objid:id});
-							
+
 			if (svgresult) {
 				SO.objectsvg = svgresult;
 			}
-			
+
 			//create svg for the links if they don't already exist
 			var links2svg = function(links){
 				forEach(links, function(link) {
@@ -222,35 +243,29 @@ var visDebug = function(){
 					}
 				});
 			};
-			links2svg(L[id].links);
-			links2svg(L[id].tolinks);
-			
+			links2svg(SO.links);
+			links2svg(SO.tolinks);
+
 			//add link xml objects and svg objects to the "to" parameter of the link
-			var links = L[id].links;
+			var links = SO.links;
 			for (var key in links) {
 				if(links.hasOwnProperty(key)){
 					var node = links[key].xmlNode;
 					var toindex = node.getAttribute('to');
-					if(!L[toindex]){
-						L[toindex] = {};
-						L[toindex].tolinks = {};
+					if(!O[toindex]){
+						O[toindex] = makeScreenObject(toindex);
 					}
-					if(!L[toindex].tolinks[key]){
-						L[toindex].tolinks[key] = {};
-					}
-					L[toindex].tolinks[key].xmlNode = node;
-					if(L[id].links[key].svg){
-						L[toindex].tolinks[key].svg = L[id].links[key].svg;
-					}
+					O[toindex].registerToLink(key, SO);
+					SO.registerFromLink(key, O[toindex]);
 				}
 			}
-			
+
 			//need to add the svg to the page! then do the z update.
 			var svgdiv = document.getElementById('svgelements');			
-			if(svgdiv !== O[id].objectsvg.parentNode){
-				svgdiv.appendChild(O[id].objectsvg);
+			if(svgdiv !== SO.objectsvg.parentNode){
+				svgdiv.appendChild(SO.objectsvg);
 			}
-			
+
 			var addLinks = function(links){
 				forEach(links, function(link){
 					if(link.svg && link.svg.node && svgdiv !== link.svg.node.parentNode){
@@ -262,81 +277,141 @@ var visDebug = function(){
 					}
 				});
 			};
-			addLinks(L[id].links);
-			addLinks(L[id].tolinks);
-			
-			zUpdate();
-			
+			addLinks(SO.links);
+			addLinks(SO.tolinks);
 
-			O[id].updatePosition('init');
-			
+			zUpdate();
+
+
+			SO.updatePosition('init');
+
+		};
+
+		SO.registerToLink = function(key, fromSO) {
+			if(!SO.tolinks[key]){
+				SO.tolinks[key] = {};
+			}
+			SO.tolinks[key].xmlNode = fromSO.links[key].xmlNode;
+			if(fromSO.links[key].svg){
+				SO.tolinks[key].svg = fromSO.links[key].svg;
+			}
+
+			if(fromSO.links[key].xmlNode.getAttribute('type').match(/Situation/)){
+
+				SO.containedObjects[fromSO.objId] = fromSO;
+				//change to contained objects, so update the targetX and targetY of all contained objects
+				var total = 0;
+				forEach(SO.containedObjects, function(){
+					total++;
+				});
+				var count = 0;
+				forEach(SO.containedObjects, function(CO){
+					CO.angle = count/total * 2 * Math.PI;
+					CO.updateTargets();
+					count++;
+				});
+			}
 		};
 		
-		SO.updatePosition = function (direction){
-			var svgelement = SO.objectsvg;
-			//check x,y and r against containing situations and contained situations...
-			var key;
-			for (key in L[id].links) {
-				if(L[id].links.hasOwnProperty(key)){
-					if(L[id].links[key].xmlNode.getAttribute('type').match(/Situation/)){
-						if (O[L[id].links[key].xmlNode.getAttribute('from')] === SO) {
+		SO.unregisterToLink = function(key, fromSO) {
+			if(fromSO.links[key].xmlNode.getAttribute('type').match(/Situation/)){
 
-							var toObj = O[L[id].links[key].xmlNode.getAttribute('to')];
+				delete SO.containedObjects[fromSO.objId];
+				//change to contained objects, so update the targetX and targetY of all contained objects
+				var total = 0;
+				forEach(SO.containedObjects, function(){
+					total++;
+				});
+				var count = 0;
+				forEach(SO.containedObjects, function(CO){
+					CO.angle = count/total * 2 * Math.PI;
+					//CO.updateTargets();
+					count++;
+				});
+			}
+		};
+		
 
-							if(direction === 'init'){
-								if(SO.r > toObj.r*3/5){
-									SO.r = toObj.r/4;
-									if(SO.r < 40){
-										SO.r = 40;
-										toObj.r = SO.r * 5/3 + 1;
-										toObj.updatePosition('up');
-									}
-								}
-							} else if(direction === 'up'){
-								if(SO.r > toObj.r*3/5){
-									toObj.r = SO.r * 5/3 + 1;
-									toObj.updatePosition('up');
-								}
-							} else{
+		SO.registerFromLink = function(key, toSO) {
+			if(SO.links[key].xmlNode.getAttribute('type').match(/Situation/)){
+				SO.containingObject = toSO;
+			}
+		};
+		
+		SO.unregisterFromLink = function(key, toSO) {
+			if(SO.links[key].xmlNode.getAttribute('type').match(/Situation/)){
+				delete SO.containingObject;
+			}
+		};
 
-								if(SO.r > toObj.r*9/10){
-									SO.r = toObj.r*9/10;
-									if(SO.r < 40){
-										SO.r = 40;
-										toObj.r = SO.r * 5/3 + 1;
-										toObj.updatePosition('up');
-									}
-								}
+		SO.updateTargets = function () {
+			//move each targetX and targetY to angle
+			if(SO.containingObject){
+				SO.targetX = SO.containingObject.x + SO.containingObject.r * 3/4 * Math.cos(SO.angle);				
+				SO.targetY = SO.containingObject.y + SO.containingObject.r * 3/4 * Math.sin(SO.angle);
+			}
+			//console.log('angle: ' + angle);
+			//console.log('cos(angle): ' + Math.cos(angle));
+		};
 
-							}
-
-							var x = SO.x - toObj.x;
-							var y = SO.y - toObj.y;
-							var r = toObj.r-SO.r;
-
-							if(Math.pow(x,2) + Math.pow(y,2) > Math.pow(r,2)){						
-								var atan = Math.atan2(y,x);
-								SO.x = toObj.x + Math.cos(atan) * Math.abs(r);
-								SO.y = toObj.y + Math.sin(atan) * Math.abs(r);
-							}
+		SO.updatePosition = function (direction) {
+			//check x,y and r against containing situations
+			if(SO.containingObject){
+				var toObj = SO.containingObject;
+				if(direction === 'init'){
+					if(SO.r > toObj.r*3/5){
+						SO.setR(toObj.r/4);
+						if(SO.r < 40){
+							SO.setR(40);
+							toObj.setR(SO.r * 5/3 + 1);
+							toObj.updatePosition('up');
 						}
 					}
+				} else if(direction === 'up'){
+					if(SO.r > toObj.r*3/5){
+						toObj.setR(SO.r * 5/3 + 1);
+						toObj.updatePosition('up');
+					}
+				} else{
+					if(SO.r > toObj.r*9/10){
+						SO.setR(toObj.r*9/10);
+						if(SO.r < 40){
+							SO.setR(40);
+							toObj.setR(SO.r * 5/3 + 1);
+							toObj.updatePosition('up');
+						}
+					}
+				}
+
+
+				var x = SO.x - toObj.x;
+				var y = SO.y - toObj.y;
+				var r = toObj.r-SO.r;
+
+				if(Math.pow(x,2) + Math.pow(y,2) > Math.pow(r,2)){						
+					var atan = Math.atan2(y,x);
+					SO.setX(toObj.x + Math.cos(atan) * Math.abs(r));
+					SO.setY(toObj.y + Math.sin(atan) * Math.abs(r));
 				}
 			}
 
-			forEach(L[id].tolinks, function(link) {
-				if(link.xmlNode.getAttribute('type').match(/Situation/)){
-					if (O[link.xmlNode.getAttribute('to')] === SO) {
-						var fromObj = O[link.xmlNode.getAttribute('from')];
-						fromObj.x += SO.x-SO.prevX;
-						fromObj.y += SO.y-SO.prevY;
-						if(direction === 'init'){
-							fromObj.updatePosition(direction);
-						} else {
-							fromObj.updatePosition();							
-						}
-					}
+
+			//check x,y and r against contained objects
+			forEach(SO.containedObjects, function(fromObj) {
+				fromObj.x += SO.x-SO.prevX;
+				fromObj.targetX = fromObj.x;
+				fromObj.y += SO.y-SO.prevY;
+				fromObj.targetY = fromObj.y;
+				if(direction === 'layout'){
+					fromObj.updateTargets();
 				}
+				
+				if(direction === 'init' || direction === 'layout'){
+					fromObj.updatePosition(direction);
+				} else {
+					fromObj.updatePosition();							
+				}
+				
 			});
 
 			SO.prevX = SO.x;
@@ -377,20 +452,58 @@ var visDebug = function(){
 					}
 				});
 			};
-			
-			updateLinkPositions(L[id].links);
-			updateLinkPositions(L[id].tolinks);
+
+			updateLinkPositions(SO.links);
+			updateLinkPositions(SO.tolinks);
+		};
+
+		SO.moveOnPath = function(){
+			var adjust = function(val, target){
+				var answer = {};
+				if(val != target){
+					answer.change = true;
+					if(Math.abs(val - target) < 10){
+						answer.val = target;
+					} else {
+						if(val < target){
+							answer.val = val + 10;
+						}else{
+							answer.val = val - 10;
+						}
+					}
+				} else {
+					answer.change = false;
+					answer.val = val;
+				}
+				return answer;
+			};
+
+			var change = false;
+			var answer;
+			answer = adjust(SO.x, SO.targetX);
+			SO.x = answer.val;
+			change = change || answer.change;
+			answer = adjust(SO.y, SO.targetY);
+			SO.y = answer.val;
+			change = change || answer.change;
+			answer = adjust(SO.r, SO.targetR);
+			SO.r = answer.val;
+			change = change || answer.change;
+
+			if(change){
+				SO.updatePosition('layout');
+			}
 		};
 
 		return SO;
 
 	};
-	
+
 
 	var zUpdate = function(){
 		//first get z-indices for all of the screenObjects
 		forEach(O, function(SO){
-			forEach(L[SO.objId].links, function(link){
+			forEach(O[SO.objId].links, function(link){
 				if(link.xmlNode.getAttribute('type').match(/Situation/)){
 					if (link.xmlNode.getAttribute('to') === SO.objId) {
 						var fromObj = O[link.xmlNode.getAttribute('from')];
@@ -401,15 +514,15 @@ var visDebug = function(){
 					}
 				}
 			});
-			
+
 		});
-		
+
 		//now check all of the objects for z-index consistency, keep iterating until no z-index changes
 		var change = true;
 		while(change){
 			change = false;
 			forEach(O, function(SO){
-				forEach(L[SO.objId].links, function(link){
+				forEach(O[SO.objId].links, function(link){
 					if(link.xmlNode.getAttribute('type').match(/Situation/)){
 						if (link.xmlNode.getAttribute('to') === SO.objId) {
 							var fromObj = O[link.xmlNode.getAttribute('from')];
@@ -422,7 +535,7 @@ var visDebug = function(){
 				});
 			});
 		}
-		
+
 		var svgdiv = document.getElementById('svgelements');
 
 		//get the current ordering from the svg
@@ -436,7 +549,7 @@ var visDebug = function(){
 				orderhash[svgObject.firstChild.id] = i;
 			}
 		});
-		
+
 		//check each object for consistency with whats on screen, fix it if there is a problem
 		forEach(O, function(SO){
 			if(SO.insertBeforeId){
@@ -448,20 +561,20 @@ var visDebug = function(){
 				}
 			}
 		});
-		
+
 	};
-	
+
 	var xmlUpdate = function(id){
-			//make a screenObject for each primitive object stemming from the root situation
-			if(!O[id]){
-				O[id] = makeScreenObject(id);
-			}
-			O[id].setObject(objectCache[id]);
+		//make a screenObject for each primitive object stemming from the root situation
+		if(!O[id]){
+			O[id] = makeScreenObject(id);
+		}
+		O[id].setObject(objectCache[id]);
 	};
-	
-	
-	
-	
+
+
+
+
 	return {
 		submitenter: function(myfield,e){
 			var keycode;
@@ -472,14 +585,14 @@ var visDebug = function(){
 			} else {
 				return true;
 			}
-			
+
 			if (keycode == 13) {
 				console.log(myfield.value);
 				var command = myfield.value;
 				myfield.value = "";
 				/*
 				var regexp = /(\w*\.)?make(\w*)\(.*\)/;
-				
+
 				var subcommand = command.match(regexp);
 
 				if(subcommand){
@@ -495,13 +608,13 @@ var visDebug = function(){
 				eval(command,initContext);
 				isNewChange = true;
 				this.rootSit2Screen();				
-				
+
 				return false;
 			} else {
 				return true;
 			}
 		},
-		
+
 		run: function(){
 			if(runcheck){
 				//alert('runcheck problem');
@@ -512,10 +625,12 @@ var visDebug = function(){
 			if (ui.drag && dragO){
 				var point = ui.calcCoord(ui.xm,ui.ym,ui.svgelements);
 				if(ui.rPressed){
-					dragO.r = 1.5*Math.sqrt(Math.pow(point.x-dragO.x,2) + Math.pow(point.y- dragO.y,2));
+					dragO.setR(1.5*Math.sqrt(Math.pow(point.x-dragO.x,2) + Math.pow(point.y- dragO.y,2)));
 				} else {
 					dragO.x = point.x - ui.initx + ui.offsetx;
+					dragO.targetX = dragO.x;
 					dragO.y = point.y - ui.inity + ui.offsety;
+					dragO.targetY = dragO.y;
 				}
 				dragO.updatePosition();
 			}
@@ -538,21 +653,33 @@ var visDebug = function(){
 				}
 				var width = ui.orig_width*ui.zoomfactor;
 				var height = ui.orig_height*ui.zoomfactor;
-				
+
 				ui.curorig_x = ui.midx - (width/2);
 				ui.curorig_y = ui.midy - (height/2);
 				ui.svgelements.setAttribute("viewBox",ui.curorig_x+" "+ui.curorig_y+" "+width+" "+height);
+			} else if (ui.lPressed){
+				forEach(O, function(SO){
+					SO.updateTargets();
+				});
+				ui.lPressed = false;
 			}
-									
+			forEach(O, function(SO){
+				SO.moveOnPath();
+			});
 			runcheck = false;
 		},
-				
-		rootSit2Screen: function(){
 
-			
+		rootSit2Screen: function(){
+			//remove all screen objects from the current objectCache
+			for (id in objectCache) {
+				if(objectCache.hasOwnProperty(id)){
+					O[id].removeObject();
+				}
+			}
+
 			//populate the objectCache by traversing the situation tree structure
 			objectCache = {};
-			
+
 			var recurseTree = function(obj){
 				objectCache[obj.getId()] = obj;
 				if(obj.getObjects){
@@ -561,11 +688,11 @@ var visDebug = function(){
 					});
 				}
 			};
-			
-			
+
+
 			recurseTree(rootSit);		
-			
-			for (var id in objectCache) {
+
+			for (id in objectCache) {
 				if(objectCache.hasOwnProperty(id)){
 					xmlUpdate(id);
 				}
@@ -575,25 +702,25 @@ var visDebug = function(){
 		},
 
 		init: function(testFunc){
-			
+
 			ui = initUserInput();
-			
+
 			//create some objects to populate the object cache
-						
+
 			var rootId = 'rootId';
 			var s = makeSituation(null, rootId);
 			rootSit = s;
-		
+
 			var myF = function(x){alert(x);};
-			
+
 			var G = {};
 			testFunc(s,G);
 			//JB.queryContent(myF, 'queryparam2');
-			
+
 			initContext = function(){};
-			
+
 			this.rootSit2Screen();
 		}
 	};
-}();
+	}();
 
