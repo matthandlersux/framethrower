2165e108e4d1949b75371b713d492464c96eae2b
tobias schachman <tschachman@tobias-schachmans-macbook-pro.local>
2009-04-27 19:16:21 -0400

    fix with javascript functions. committing my test files (tobytest).

---
+++ client/templates/bin/tobytest.js
@@ -0,0 +1,153 @@
+var mainTemplate = {
+	kind: "templateCode",
+	params: [
+
+	],
+	type: "XMLP",
+	let: {
+		test: {
+			kind: "lineExpr",
+			expr: "88",
+			let: {
+
+			}
+		},
+		myTemplate: {
+			kind: "lineTemplate",
+			template: {
+				kind: "templateCode",
+				params: [
+					"x"
+				],
+				type: "Number -> XMLP",
+				let: {
+
+				},
+				output: {
+					kind: "lineXML",
+					xml: {
+						kind: "element",
+						nodeName: "div",
+						attributes: {
+
+						},
+						style: {
+
+						},
+						children: [
+							{
+								kind: "textElement",
+								nodeValue: "Here's another template that has access to scope, see:"
+							},
+							{
+								kind: "textElement",
+								nodeValue: {
+									kind: "insert",
+									expr: "test"
+								}
+							},
+							{
+								kind: "textElement",
+								nodeValue: " , and here's a parameter:"
+							},
+							{
+								kind: "textElement",
+								nodeValue: {
+									kind: "insert",
+									expr: "x"
+								}
+							}
+						]
+					}
+				}
+			}
+		},
+		jsfun: {
+			kind: "lineJavascript",
+			type: "Number -> t0",
+			f: function (x) {return x + 99 ;}
+		}
+	},
+	output: {
+		kind: "lineXML",
+		xml: {
+			kind: "element",
+			nodeName: "div",
+			attributes: {
+				testatt: {
+					kind: "insert",
+					expr: "test"
+				}
+			},
+			style: {
+
+			},
+			children: [
+				{
+					kind: "textElement",
+					nodeValue: "\n		Hello World.\n		"
+				},
+				{
+					kind: "for-each",
+					select: "testCell",
+					templateCode: {
+						kind: "templateCode",
+						params: [
+							"entry"
+						],
+						type: "t0 -> XMLP",
+						let: {
+
+						},
+						output: {
+							kind: "lineXML",
+							xml: {
+								kind: "element",
+								nodeName: "div",
+								attributes: {
+
+								},
+								style: {
+
+								},
+								children: [
+									{
+										kind: "textElement",
+										nodeValue: "An entry:"
+									},
+									{
+										kind: "textElement",
+										nodeValue: {
+											kind: "insert",
+											expr: "entry"
+										}
+									}
+								]
+							}
+						}
+					}
+				},
+				{
+					kind: "call",
+					templateCode: {
+						kind: "templateCode",
+						params: [
+
+						],
+						type: "XMLP",
+						let: {
+
+						},
+						output: {
+							kind: "lineExpr",
+							expr: "myTemplate ( jsfun 9999 )",
+							let: {
+
+							}
+						}
+					}
+				}
+			]
+		}
+	}
+};
---
+++ client/templates/tobytest/tobytest.tpl
@@ -0,0 +1,16 @@
+template () {
+	test = 88,
+	myTemplate = template (x::Number) {
+		<div>Here's another template that has access to scope, see: {test} , and here's a parameter: {x}</div>
+	},
+	jsfun = function (x::Number) {
+		return x + 99;
+	},
+	<div testatt="{test}">
+		Hello World.
+		<f:each select="testCell" key="entry">
+			<div>An entry: {entry}</div>
+		</f:each>
+		<f:call>myTemplate (jsfun 9999)</f:call>
+	</div>
+}
