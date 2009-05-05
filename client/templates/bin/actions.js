44077cf10309b280f7909b10c8f530ce881055d0
Andrew Dailey <andrewdailey@andrew.(none)>
2009-05-05 19:45:58 -0400

    Parser now should be outputting correct JSON for actions.

--- client/templates/bin/actions.js
+++ client/templates/bin/actions.js
@@ -5,13 +5,293 @@ var mainTemplate = {
 	],
 	type: "Set Object -> XMLP",
 	let: {
-		putObjectInSituation: "action",
-		makeTrueInOnt: "action",
-		makeCons: "action",
-		makeInfon2: "action",
-		makeOntProperty: "action",
-		nameObject: "action",
-		changeName: "action",
+		putObjectInSituation: {
+			kind: "action",
+			params: [
+				"object",
+				"situation"
+			],
+			type: "Object -> Object -> ACTION",
+			actions: [
+				{
+					name: "in",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "shared.in",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: "infon",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "makeInfon2 in situation object",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: "truth",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "Cons::truth infon",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "actionUpdate",
+						target: "truth",
+						actionType: "add",
+						key: "null",
+						val: undefined
+					}
+				}
+			]
+		},
+		makeTrueInOnt: {
+			kind: "action",
+			params: [
+				"infon"
+			],
+			type: "Cons -> ACTION",
+			actions: [
+				{
+					name: "ont",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "shared.ont",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "putObjectInSituation infon ont",
+						let: {
+
+						}
+					}
+				}
+			]
+		},
+		makeCons: {
+			kind: "action",
+			params: [
+				"left",
+				"right"
+			],
+			type: "Object -> Object -> ACTION",
+			actions: [
+				{
+					name: "cons",
+					type: undefined,
+					value: {
+						kind: "actionCreate",
+						type: "Cons",
+						prop: "{left : left , right : right}"
+					}
+				},
+				{
+					name: "upRight",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "Object::upRight left",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: "upLeft",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "Object::upLeft right",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "actionUpdate",
+						target: "upRight",
+						actionType: "add",
+						key: "cons",
+						val: undefined
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "actionUpdate",
+						target: "upLeft",
+						actionType: "add",
+						key: "cons",
+						val: undefined
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "cons",
+						let: {
+
+						}
+					}
+				}
+			]
+		},
+		makeInfon2: {
+			kind: "action",
+			params: [
+				"relation",
+				"arg1",
+				"arg2"
+			],
+			type: "Object -> Object -> Object -> ACTION",
+			actions: [
+				{
+					name: "cons1",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "makeCons relation arg1",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "makeCons cons1 arg2",
+						let: {
+
+						}
+					}
+				}
+			]
+		},
+		makeOntProperty: {
+			kind: "action",
+			params: [
+				"relation",
+				"arg1",
+				"arg2"
+			],
+			type: "Object -> Object -> Object -> ACTION",
+			actions: [
+				{
+					name: "infon",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "makeInfon2 relation arg1 arg2",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "makeTrueInOnt infon",
+						let: {
+
+						}
+					}
+				}
+			]
+		},
+		nameObject: {
+			kind: "action",
+			params: [
+				"object",
+				"name"
+			],
+			type: "Object -> String -> ACTION",
+			actions: [
+				{
+					name: "nameRel",
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "shared.name",
+						let: {
+
+						}
+					}
+				},
+				{
+					name: "nameText",
+					type: undefined,
+					value: {
+						kind: "actionCreate",
+						type: "X.text",
+						prop: "{string : name}"
+					}
+				},
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "makeOntProperty nameRel object nameText",
+						let: {
+
+						}
+					}
+				}
+			]
+		},
+		changeName: {
+			kind: "action",
+			params: [
+				"focus",
+				"newName"
+			],
+			type: "Object -> String -> ACTION",
+			actions: [
+				{
+					name: undefined,
+					type: undefined,
+					value: {
+						kind: "lineExpr",
+						expr: "nameObject focus newName",
+						let: {
+
+						}
+					}
+				}
+			]
+		},
 		message: {
 			kind: "lineExpr",
 			expr: ""This is a new name"",
@@ -51,7 +331,24 @@ var mainTemplate = {
 						output: {
 							kind: "lineXML",
 							xml: {
-								event: "click"
+								kind: "on",
+								event: "click",
+								templateCode: {
+									kind: "action",
+									params: [
+
+									],
+									type: "ACTION",
+									actions: [
+										{
+											kind: "lineExpr",
+											expr: "changeName key message",
+											let: {
+
+											}
+										}
+									]
+								}
 							}
 						}
 					}
