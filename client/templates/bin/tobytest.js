d50af0ef654c7971958b77a53ad59aeb6bb85036
Andrew Dailey <andrewdailey@andrew.(none)>
2009-05-12 12:55:10 -0400

    Fixed assembler.js to put lets in linestatement and put json keys in single quotes to handle javascript reserved words

--- client/templates/bin/tobytest.js
+++ client/templates/bin/tobytest.js
@@ -1,60 +1,60 @@
 var mainTemplate = {
-	kind: "templateCode",
-	params: [
+	'kind': "templateCode",
+	'params': [
 
 	],
-	type: "XMLP",
-	let: {
-		test: {
-			kind: "lineExpr",
-			expr: "88",
-			let: {
+	'type': "XMLP",
+	'let': {
+		'test': {
+			'kind': "lineExpr",
+			'expr': "88",
+			'let': {
 
 			}
 		},
-		myTemplate: {
-			kind: "lineTemplate",
-			template: {
-				kind: "templateCode",
-				params: [
+		'myTemplate': {
+			'kind': "lineTemplate",
+			'template': {
+				'kind': "templateCode",
+				'params': [
 					"x"
 				],
-				type: "Number -> XMLP",
-				let: {
+				'type': "Number -> XMLP",
+				'let': {
 
 				},
-				output: {
-					kind: "lineXML",
-					xml: {
-						kind: "element",
-						nodeName: "div",
-						attributes: {
+				'output': {
+					'kind': "lineXML",
+					'xml': {
+						'kind': "element",
+						'nodeName': "div",
+						'attributes': {
 
 						},
-						style: {
+						'style': {
 
 						},
-						children: [
+						'children': [
 							{
-								kind: "textElement",
-								nodeValue: "Here's another template that has access to scope, see: "
+								'kind': "textElement",
+								'nodeValue': "Here's another template that has access to scope, see: "
 							},
 							{
-								kind: "textElement",
-								nodeValue: {
-									kind: "insert",
-									expr: "test"
+								'kind': "textElement",
+								'nodeValue': {
+									'kind': "insert",
+									'expr': "test"
 								}
 							},
 							{
-								kind: "textElement",
-								nodeValue: " , and here's a parameter: "
+								'kind': "textElement",
+								'nodeValue': " , and here's a parameter: "
 							},
 							{
-								kind: "textElement",
-								nodeValue: {
-									kind: "insert",
-									expr: "x"
+								'kind': "textElement",
+								'nodeValue': {
+									'kind': "insert",
+									'expr': "x"
 								}
 							}
 						]
@@ -62,141 +62,258 @@ var mainTemplate = {
 				}
 			}
 		},
-		jsfun: {
-			kind: "lineJavascript",
-			type: "Number -> t0",
-			f: function (x) {return x + 99 ;}
+		'jsfun': {
+			'kind': "lineJavascript",
+			'type': "Number -> t0",
+			'f': function (x) {return x + 99 ;}
 		},
-		stateTest: {
-			kind: "lineState",
-			type: "Unit Number"
-		},
-		changeState: {
-			kind: "lineAction",
-			action: {
-				kind: "action",
-				params: [
-					"newValue"
+		'counter': {
+			'kind': "lineTemplate",
+			'template': {
+				'kind': "templateCode",
+				'params': [
+
 				],
-				type: "Number -> Action",
-				actions: [
-					{
-						action: {
-							kind: "actionUpdate",
-							target: "stateTest",
-							actionType: "add",
-							key: "newValue",
-							val: undefined
+				'type': "XMLP",
+				'let': {
+					'counterValue': {
+						'kind': "lineState",
+						'type': "Unit Number"
+					},
+					'changeState': {
+						'kind': "lineAction",
+						'action': {
+							'kind': "action",
+							'params': [
+								"newValue"
+							],
+							'type': "Number -> Action",
+							'actions': [
+								{
+									'action': {
+										'kind': "actionUpdate",
+										'target': "counterValue",
+										'actionType': "add",
+										'key': "newValue",
+										'val': undefined
+									}
+								}
+							]
 						}
 					}
-				]
-			}
-		},
-		counter: {
-			kind: "templateCode",
-			params: [
+				},
+				'output': {
+					'kind': "lineXML",
+					'xml': {
+						'kind': "element",
+						'nodeName': "div",
+						'attributes': {
+							'testatt': {
+								'kind': "insert",
+								'expr': "test"
+							}
+						},
+						'style': {
 
-			],
-			type: "XMLP",
-			let: {
+						},
+						'children': [
+							{
+								'kind': "element",
+								'nodeName': "div",
+								'attributes': {
 
-			},
-			output: {
-				kind: "lineXML",
-				xml: {
-					kind: "element",
-					nodeName: "div",
-					attributes: {
+								},
+								'style': {
 
-					},
-					style: {
+								},
+								'children': [
+									{
+										'kind': "textElement",
+										'nodeValue': "\n			Counter value: "
+									},
+									{
+										'kind': "textElement",
+										'nodeValue': {
+											'kind': "insert",
+											'expr': "counterValue"
+										}
+									}
+								]
+							},
+							{
+								'kind': "element",
+								'nodeName': "div",
+								'attributes': {
 
-					},
-					children: [
-						{
-							kind: "textElement",
-							nodeValue: "\n		just a test\n	"
-						}
-					]
+								},
+								'style': {
+
+								},
+								'children': [
+									{
+										'kind': "on",
+										'event': "click",
+										'action': {
+											'kind': "action",
+											'params': [
+
+											],
+											'type': "Action",
+											'actions': [
+												{
+													'action': {
+														'kind': "lineExpr",
+														'expr': "changeState 0",
+														'let': {
+
+														}
+													}
+												}
+											]
+										}
+									},
+									{
+										'kind': "textElement",
+										'nodeValue': "\n			Reset Counter\n		"
+									}
+								]
+							},
+							{
+								'kind': "element",
+								'nodeName': "div",
+								'attributes': {
+
+								},
+								'style': {
+
+								},
+								'children': [
+									{
+										'kind': "for-each",
+										'select': "counterValue",
+										'templateCode': {
+											'kind': "templateCode",
+											'params': [
+												"currentState"
+											],
+											'type': "t0 -> XMLP",
+											'let': {
+
+											},
+											'output': {
+												'kind': "lineXML",
+												'xml': {
+													'kind': "on",
+													'event': "click",
+													'action': {
+														'kind': "action",
+														'params': [
+
+														],
+														'type': "Action",
+														'actions': [
+															{
+																'action': {
+																	'kind': "lineExpr",
+																	'expr': "changeState ( plus currentState 1 )",
+																	'let': {
+
+																	}
+																}
+															}
+														]
+													}
+												}
+											}
+										}
+									},
+									{
+										'kind': "textElement",
+										'nodeValue': "\n			Increment Counter\n		"
+									}
+								]
+							}
+						]
+					}
 				}
 			}
 		}
 	},
-	output: {
-		kind: "lineXML",
-		xml: {
-			kind: "element",
-			nodeName: "div",
-			attributes: {
-				testatt: {
-					kind: "insert",
-					expr: "test"
+	'output': {
+		'kind': "lineXML",
+		'xml': {
+			'kind': "element",
+			'nodeName': "div",
+			'attributes': {
+				'testatt': {
+					'kind': "insert",
+					'expr': "test"
 				}
 			},
-			style: {
+			'style': {
 
 			},
-			children: [
+			'children': [
 				{
-					kind: "textElement",
-					nodeValue: "\n		Hello World.\n		"
+					'kind': "textElement",
+					'nodeValue': "\n		Hello World.\n		"
 				},
 				{
-					kind: "element",
-					nodeName: "div",
-					attributes: {
+					'kind': "element",
+					'nodeName': "div",
+					'attributes': {
 
 					},
-					style: {
+					'style': {
 
 					},
-					children: [
+					'children': [
 						{
-							kind: "textElement",
-							nodeValue: "\n			Set test: "
+							'kind': "textElement",
+							'nodeValue': "\n			Set test: "
 						},
 						{
-							kind: "textElement",
-							nodeValue: {
-								kind: "insert",
-								expr: "testCell"
+							'kind': "textElement",
+							'nodeValue': {
+								'kind': "insert",
+								'expr': "testCell"
 							}
 						}
 					]
 				},
 				{
-					kind: "for-each",
-					select: "testCell",
-					templateCode: {
-						kind: "templateCode",
-						params: [
+					'kind': "for-each",
+					'select': "testCell",
+					'templateCode': {
+						'kind': "templateCode",
+						'params': [
 							"entry"
 						],
-						type: "t0 -> XMLP",
-						let: {
+						'type': "t0 -> XMLP",
+						'let': {
 
 						},
-						output: {
-							kind: "lineXML",
-							xml: {
-								kind: "element",
-								nodeName: "div",
-								attributes: {
+						'output': {
+							'kind': "lineXML",
+							'xml': {
+								'kind': "element",
+								'nodeName': "div",
+								'attributes': {
 
 								},
-								style: {
+								'style': {
 
 								},
-								children: [
+								'children': [
 									{
-										kind: "textElement",
-										nodeValue: "An entry: "
+										'kind': "textElement",
+										'nodeValue': "An entry: "
 									},
 									{
-										kind: "textElement",
-										nodeValue: {
-											kind: "insert",
-											expr: "entry"
+										'kind': "textElement",
+										'nodeValue': {
+											'kind': "insert",
+											'expr': "entry"
 										}
 									}
 								]
@@ -205,50 +322,50 @@ var mainTemplate = {
 					}
 				},
 				{
-					kind: "call",
-					templateCode: {
-						kind: "templateCode",
-						params: [
+					'kind': "call",
+					'templateCode': {
+						'kind': "templateCode",
+						'params': [
 
 						],
-						type: "XMLP",
-						let: {
+						'type': "XMLP",
+						'let': {
 
 						},
-						output: {
-							kind: "lineExpr",
-							expr: "myTemplate ( jsfun 9999 )",
-							let: {
+						'output': {
+							'kind': "lineExpr",
+							'expr': "myTemplate ( jsfun 9999 )",
+							'let': {
 
 							}
 						}
 					}
 				},
 				{
-					kind: "element",
-					nodeName: "div",
-					attributes: {
+					'kind': "element",
+					'nodeName': "div",
+					'attributes': {
 
 					},
-					style: {
+					'style': {
 
 					},
-					children: [
+					'children': [
 						{
-							kind: "call",
-							templateCode: {
-								kind: "templateCode",
-								params: [
+							'kind': "call",
+							'templateCode': {
+								'kind': "templateCode",
+								'params': [
 
 								],
-								type: "XMLP",
-								let: {
+								'type': "XMLP",
+								'let': {
 
 								},
-								output: {
-									kind: "lineExpr",
-									expr: "counter",
-									let: {
+								'output': {
+									'kind': "lineExpr",
+									'expr': "counter",
+									'let': {
 
 									}
 								}
