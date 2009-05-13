3be62a9aba5a7a385b4a1e6d14c6547254d4a8fa
tobias schachman <tschachman@tobias-schachmans-macbook-pro.local>
2009-05-13 15:10:25 -0400

    added trigger to syntax/semantics

--- client/js/templates/style.js
+++ client/js/templates/style.js
@@ -3,13 +3,21 @@
 
 
 
-
+var styleNamesThatTakePx = {
+	"borderBottomWidth":true, "borderLeftWidth":true, "borderRightWidth":true, "borderSpacing":true, "borderTopWidth":true,
+	"bottom":true, "fontSize":true, "height":true, "left":true, "letterSpacing":true, "lineHeight":true,
+	"margin":true, "marginBottom":true, "marginLeft":true, "marginRight":true, "marginTop":true,
+	"maxHeight":true, "maxWidth":true, "minHeight":true, "minWidth":true,
+	"MozBorderRadius":true, "MozBorderRadiusBottomleft":true, "MozBorderRadiusBottomright":true, "MozBorderRadiusTopleft":true, "MozBorderRadiusTopright":true,
+	"outlineWidth":true, "padding":true, "paddingBottom":true, "paddingLeft":true, "paddingRight":true, "paddingTop":true,
+	"right":true, "top":true, "width":true
+};
 
 function setNodeStyle(node, styleName, styleValue) {
 	// TODO this will need some additional code for convenience/browser bullshit (px, etc)
 	// https://developer.mozilla.org/en/DOM/CSS
 
-	node.style[styleName] = styleValue + "px";
+	node.style[styleName] = styleValue + (styleNamesThatTakePx[styleName] ? "px" : "");
 }
 
 
--- client/templates/tobytest/dragging.tpl
+++ client/templates/tobytest/dragging.tpl
@@ -6,7 +6,21 @@ template () {
 	stopDrag = action () {
 		remove(dragging)
 	},
-	<div>
+	currentPosX = state(Unit Number),
+	currentPosY = state(Unit Number),
+	
+	<div style-position="absolute" style-left="{UI.ui:mouseX ui.ui}" style-top="{UI.ui:mouseY ui.ui}">
+		<f:on event="click">startDrag</f:on>
+		<f:each select="dragging" key="blah">
+			<div>blah</div>
+			// <f:on trigger="UI.ui:mouseX ui.ui" as="mouseX">
+			// 	add(currentPosX, mouseX)
+			// </f:on>
+			// <f:on trigger="UI.ui:mouseY ui.ui" as="mouseY">
+			// 	add(currentPosY, mouseY)
+			// </f:on>
+			// <f:on trigger="reactiveNot UI.ui:mouseDown ui.ui">stopDrag</f:on>
+		</f:each>
 		Draggin {UI.ui:mouseX ui.ui} {UI.ui:mouseY ui.ui}
 	</div>
 }
