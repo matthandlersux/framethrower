6b6ee8ed3d360deda83a152e2d162c7163935b30
tobias schachman <tschachman@tobias-schachmans-macbook-pro.local>
2009-07-01 15:02:57 -0400

    documentation for fetch project in preparse.js

--- client/source/js/templates/preparse.js
+++ client/source/js/templates/preparse.js
@@ -49,22 +49,32 @@ function preparse(template) {
 
 
 
-function desugarFetch(template) {
+/*
+this function takes an AST (ie: as returned by parse()) and returns an AST
+	where all instances of fetch have been removed and various mapUnit's have been added.
+examples:
+	fetch x => x			[ or maybe this should be: mU1 (x0 -> x0) x ]
+	plus (fetch x) 6 => mU1 (x0 -> plus x0 6) x
+	plus (fetch x) (fetch y) => mU2 (x0 -> x1 -> plus x0 x1) x y
+	plus (fetch x) (fetch x) => mU1 (x0 -> plus x0 x0) x
+	plus (plus (fetch x) 6) 8 => mU1 (x0 -> plus (plus x0 6) 8) x
+
+Notice that if expr has type a, then the removeFetch version of expr will have type Unit (Unit (... (Unit a))) where the number of Unit's is equal to the number of nested fetch's.
+But in practice, I don't expect to ever see more than one layer of fetch's
+*/
+function removeFetch(ast) {
 	
-	/*
-	this function takes an AST (ie: as returned by parse()) and returns an AST
-		where all instances of fetch have been removed and various mapUnit's have been added.
-	examples:
-		fetch x => x			[ or maybe this should be: mU1 (x0 -> x0) x ]
-		plus (fetch x) 6 => mU1 (x0 -> plus x0 6) x
-		plus (fetch x) (fetch y) => mU2 (x0 -> x1 -> plus x0 x1) x y
-		plus (fetch x) (fetch x) => mU1 (x0 -> plus x0 x0) x
-		plus (plus (fetch x) 6) 8 => mU1 (x0 -> plus (plus x0 6) 8) x
-	
-	Notice that if 
-	*/
-	function removeFetch(ast) {
-		
-	}
+}
+
+
+/*
+this function goes through the template tree (see doc/template semantics.txt)
+it keeps an environment mapping words (VTCs) to their expressions, but only if the expressions have fetch
+whenever it encounters a new expression, it checks to see if it has fetch or makes reference to a word that has fetch
+	if so, it replaces the word(s) with its expression(s) in the env, and replaces the expression in the template tree with its removeFetch'ed version
+*/
+function desugarFetch(template, env) {
+	if (!env) env = falseEnv; // or maybe, to be clever, the starting environment should just be the word "fetch" mapped to "fetch"
+
 	
 }
