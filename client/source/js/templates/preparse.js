e3fa347258bc350e2aab523fc1e9cf8bd679185c
tobias schachman <tschachman@tobias-schachmans-macbook-pro.local>
2009-07-01 14:46:18 -0400

    writing up documentation on preparse

--- client/source/js/templates/preparse.js
+++ client/source/js/templates/preparse.js
@@ -1,5 +1,14 @@
+/*
+Pipeline:
+templates start out as .tpl and .let files
+compiler (tools/compiler) takes these and turns them into .js files
+next is preparse which just runs parse on all expressions (String -> AST), and parseType on all types (String -> TYPE)
+next is desugarFetch which will rewrite some expressions so that a fetch is never encountered
+finally the interpreter (js/templates/closure.js, action.js, writexml.js) interprets the result of all this
+*/
+
+
 function preparse(template) {
-	//TODO: move this functionality into semantics.js
 	if (!template) return;
 	
 	var kind = template.kind;
@@ -12,8 +21,6 @@ function preparse(template) {
 		}
 	} else if (kind === "lineJavascript") {
 		template.type = parseType(template.type);
-	} else if (kind === "lineState") {
-		//template.type = parseType(template.type);
 	} else if (kind === "lineAction") {
 		template.type = parseType(template.type);
 	} else if (kind === "actionCreate") {
@@ -37,4 +44,27 @@ function preparse(template) {
 	if (arrayLike(template) || objectLike(template)) {
 		forEach(template, preparse);
 	}
+}
+
+
+
+
+function desugarFetch(template) {
+	
+	/*
+	this function takes an AST (ie: as returned by parse()) and returns an AST
+		where all instances of fetch have been removed and various mapUnit's have been added.
+	examples:
+		fetch x => x			[ or maybe this should be: mU1 (x0 -> x0) x ]
+		plus (fetch x) 6 => mU1 (x0 -> plus x0 6) x
+		plus (fetch x) (fetch y) => mU2 (x0 -> x1 -> plus x0 x1) x y
+		plus (fetch x) (fetch x) => mU1 (x0 -> plus x0 x0) x
+		plus (plus (fetch x) 6) 8 => mU1 (x0 -> plus (plus x0 6) 8) x
+	
+	Notice that if 
+	*/
+	function removeFetch(ast) {
+		
+	}
+	
 }
