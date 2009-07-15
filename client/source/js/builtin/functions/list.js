138784e0a1ec5d7e447066dea2704ccd27c3359c
tobias schachman <tschachman@tobias-schachmans-macbook-pro.local>
2009-07-15 15:38:58 -0400

    ontology tester, working on adding list datatype

--- client/source/js/builtin/functions/list.js
+++ client/source/js/builtin/functions/list.js
@@ -21,6 +21,26 @@
 		};
 	}
 	
+	function head(list) {
+		if (list.asArray.length === 0) {
+			debug.error("Trying to take head of empty list");
+		}
+		return list.asArray[0];
+	}
+	function tail(list) {
+		if (list.asArray.length === 0) {
+			debug.error("Trying to take tail of empty list");
+		}
+		return {
+			kind: "list",
+			type: list.type,
+			remote: 2,
+			asArray: list.asArray.slice(1)
+		};
+	}
+	
 	base.add("nil", nil);
 	addFun("cons", "a -> List a -> List a", cons);
+	addFun("head", "List a -> a", head);
+	addFun("tail", "List a -> List a", tail);
 })();
