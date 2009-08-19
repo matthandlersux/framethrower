61011c17e541f425de2dd0cd8a7f3d0a6a34d70c
Matt Handler <matt.handler@gmail.com>
2009-08-19 13:46:53 -0400

    updates to cellstate, still very tentative

--- server/mbrella/src/cellState.erl
+++ server/mbrella/src/cellState.erl
@@ -10,6 +10,12 @@
 -endif.
 
 %% ====================================================
+%% notes
+%% ====================================================
+
+% all functions in the api should return at least #cellState, possibly a message too
+
+%% ====================================================
 %% TYPES
 %% ====================================================
 
@@ -26,11 +32,63 @@ injectOutput(State, OutputFunction, OutputTo) ->
 	State1 = addOutput(State, OutputFunction),
 	connectOutput(State, OutputFunction, OutputTo).
 
+%% 
+%% addElement :: #cellState -> #element -> {ok, #cellState} | {first, #cellState}
+%% 
+
+addElement(State, Element) -> 
+	Elements = state(State, elements),
+	CellType = state(State, cellType),
+	case elementsAdd(CellType, Element, Elements) of
+		{ok, Elements1} -> {ok, replace(State, elements, Elements1)};
+		{first, Elements1} -> {first, replace(State, elements, Elements1)}
+	end.
+
+%% 
+%% removeElement :: #cellState -> #element -> {ok, #cellState} | {last, #cellState}
+%% 
+	
+removeElement() -> 
+	Elements = state(State, elements),
+	case elementsRemove(Element, Elements) of
+		{ok, Elements1} -> {ok, replace(State, elements, Elements1)};
+		{last, Elements1} -> {last, replace(State, elements, Elements1)}
+	end.
+	
+%% 
+%% interceptElements :: #cellState -> CellName -> Elements -> {NewState, NewElements}
+%% 
+% interceptFunctions :: List Args -> State -> List Message -> {ok, NewState, List Elements}
+
+interceptElements(CellState, FromName, Elements) ->
+	Intercept = state(State, intercept),
+	{ok, NewIntercept, Elements1} = runInterceptOnElements(),
+	doElements()
+	
 %% ====================================================
 %% Internal API
 %% ====================================================
 
+elementsAdd(unit, Element, Elements) ->
+	case rangedict:find(Element, Elements) of
+		{ok, FoundElement} ->
+			rangedict:addWeight(Element, Elements);
+		error -> rangedict:add(Element, Elements)
+	end;
+elementsAdd(set, Element, Elements) ->
+	case rangedict:find(Element, Elements) of
+		{ok, FoundElement} ->
+			rangedict:addWeight(Element, Elements);
+		error -> rangedict:add(Element, Elements)
+	end;
+elementsAdd(map, Element, Elements) ->
+	case rangedict:find(Element, Elements) of
+		{ok, FoundElement} ->
+			rangedict:addWeight(Element, Elements);
+		error -> rangedict:add(Element, Elements)
+	end.
 
+	
 %% ====================================================
 %% Utilities
 %% ====================================================
