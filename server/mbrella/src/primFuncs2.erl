bf1bf35deb23cad68958d7f21f4b1317252e4fdb
Matt Handler <matt.handler@gmail.com>
2009-08-19 14:03:02 -0400

    return primfuncs to previous state as primfuncs2 module

--- server/mbrella/src/primFuncs2.erl
+++ server/mbrella/src/primFuncs2.erl
@@ -294,16 +294,6 @@ reactiveAnd(Cell1, Cell2) ->
 	end),
 	cell:unleash(OutputCell),
 	OutputCell.
-
-reactiveAnd(Cell1, Cell2) ->
-	OutputCell = cell:makeLeashedCell(),
-	InterceptKeys = cell:makeInterceptKeys(Cell1, Cell2),
-	InterceptState = {undefined, undefined, undefined}
-	%need to designate that Cell1 -> reactiveAnd's Cell1 always
-	cell:createIntercept(OutputCell, InterceptKeys, funs:pointer(intercepts, reactiveAnd, [state, key]), State),
-	cell:unleash(OutputCell).
-	
-intercepts:reactiveAnd(State, Message) ->
 	
 %% 
 %% reactiveOr:: Unit Null -> Unit Null -> Unit Null
@@ -374,11 +364,6 @@ isEmpty(Cell) ->
 	end),
 	OutputCell.
 
-isEmpty(Cell) ->
-	OutputCell = cell:makeLeashedCell(),
-	cell:addElement(OutputCell, null),
-	cell:injectIntercept(OutputCell, {switch})
-	cell:inject(Cell, OutputCell, send).
 %% 
 %% gate:: Unit b -> a -> Unit a
 %% 
@@ -429,29 +414,6 @@ fold(Fun, FunInv, Init, Cell) ->
 		fun() -> intercept:sendIntercept(Intercept, {minus, Val}) end
 	end),
 	OutputCell.
-
-fold(Cell, Function, FunctionInverse, InitialValue) ->
-	OutputCell = cell:makeLeashedCell(unit),
-	cell:addElement(OutputCell, InitialValue),
-	cell:injectIntercept(OutputCell, {interceptFold, InitialValue, [Function, FunctionInverse]}),
-	cell:inject(Cell, OutputCell, send),
-	cell:unleash(OutputCell).
-	
-% -get message [{add, value1},{add, value2},{add, value3}] or [{add,value1}] or [{remove, value3}]
-% -check if have intercept
-% -if so, call intercept function with arguments, extra arguments are message and state
-% 	-function returns {newstate, newmessage}
-% -update interceptstate, send newmessage to elements
-%
-% interceptFunctions :: List Args -> State -> List Message -> {ok, NewState, List Elements}
-
-interceptFold(_, _, _, []) ->
-	[];
-interceptFold(Function, FunctionInverse, InitialValue, [H|T]) ->
-	[interceptFold(Function, FunctionInverse, InitialValue, H)]
-interceptFold(Function, _, InitialValue, {add, Value}) ->
-	;
-interceptFold(_, FunctionInverse, InitialValue, {remove, Value}) ->
 	
 
 unfoldSet(Fun, Init) ->
