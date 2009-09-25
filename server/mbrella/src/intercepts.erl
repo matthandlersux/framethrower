3ab97ae3cc8b86050626f941c0e0499044253e17
Matt Handler <matt.handler@gmail.com>
2009-09-25 13:58:24 -0400

    updated intercepts to deal with only elements and have a from as a separate parameter

--- server/mbrella/src/intercepts.erl
+++ server/mbrella/src/intercepts.erl
@@ -1,7 +1,7 @@
 -module (intercepts.erl).
 -compile(export_all).
 
--include().
+% -include().
 
 -ifdef( debug ).
 -define( trace(X), io:format("TRACE ~p:~p ~p~n", [?MODULE, ?LINE, X]) ).
@@ -28,25 +28,31 @@
 %% callIntercept :: String -> List a -> b -> List Message -> Tuple b (List Element)
 %% 
 
+call(Intercept, From, Elements) ->
+	Name = getName(Intercept),
+	Args = getArgs(Intercept),
+	State = getState(Intercept),
+	callIntercept(Name, Args, State, From, Elements)
+
 callIntercept(_, _, _, []) -> {undefined, []};
-callIntercept(Name, Args, State, Messages) ->
-	Process = fun(Message, {OldState, OldElements}) ->
-		{NewState, Elements} = processMessage(Name, Args, OldState, Message),
+callIntercept(Name, Args, State, From, Elements) ->
+	Process = fun(Element, {OldState, OldElements}) ->
+		{NewState, Elements} = processMessage(Name, Args, OldState, From, Element),
 		{NewState, Elements ++ OldElements}
 	end,
-	lists:foldl(Process, {State, []}, Messages).
+	lists:foldl(Process, {State, []}, Elements).
 
-callIntercept(Name, Messages) ->
-	callIntercept(Name, [], [], Messages).
+callIntercept(Name, From, Elements) ->
+	callIntercept(Name, [], [], From, Elements).
 	
-callIntercept(Name, Args, Messages) ->
-	callIntercept(Name, Args, [], Messages).
+callIntercept(Name, Args, From, Elements) ->
+	callIntercept(Name, Args, [], From, Elements).
 %% ====================================================
 %% Internal API
 %% ====================================================
 
-processMessage(Name, Args, State, Message) ->
-	case erlang:apply(intercepts, Name, Args ++ State ++ Message) of
+processMessage(Name, Args, State, From, Element) ->
+	case erlang:apply(intercepts, Name, Args ++ State ++ From ++ Element) of
 		{NewState, Elements} when is_list(Elements) ->
 			{NewState, Elements};
 		{NewState, Elements} when is_tuple(Elements) ->
@@ -63,26 +69,28 @@ processMessage(Name, Args, State, Message) ->
 %
 %	interceptFunction :: Args -> InterceptState -> KeyedMessages -> Tuple InterceptState UnkeyedMessages
 
-fold( Function, FunctionInverse, InterceptState, ListOfMessages ) ->
+fold( Function, FunctionInverse, InterceptState, From, Element ) ->
 	
-	{NewInterceptState, ElementsToAdd}.
+	{todo_NewInterceptState, todo_ElementsToAdd}.
 	
 %% 
 %% reactiveAnd ::
 %%	Message will look like {add, FromCellPointer, null}
 %% 
 
-reactiveAnd( CellPointer1, CellPointer2, {Cell1Val, Cell2Val}, Message ) ->
-	case cellMessage:fromName(Message) of
-		cellPointer:name(CellPointer1) -> 
-			Cell1NewVal = cellMessage:modifier(Message),
+reactiveAnd( CellPointer1, CellPointer2, {Cell1Val, Cell2Val}, From, Element ) ->
+	CellName1 = cellPointer:name(CellPointer1),
+	CellName2 = cellPointer:name(CellPointer2),
+	case cellPointer:name(From) of
+		CellName1 -> 
+			Cell1NewVal = cellElement:modifier(Element),
 			if 
 				Cell2Val =:= undefined -> { {Cell1NewVal, Cell2Val}, [] };
 				Cell1NewVal =:= add andalso Cell2Val =:= add -> { {Cell1NewVal, Cell2Val}, {add, null} };
 				true -> { {Cell1NewVal, Cell2Val}, {remove, null} }
 			end;
-		cellPointer:name(CellPointer2) ->
-			Cell2NewVal = cellMessage:modifier(Message),
+		CellName2 ->
+			Cell2NewVal = cellElement:modifier(Element),
 			if 
 				Cell1Val =:= undefined -> { {Cell1Val, Cell2NewVal}, [] };
 				Cell1Val =:= add andalso Cell2NewVal =:= add -> { {Cell1Val, Cell2NewVal}, {add, null} };
@@ -90,39 +98,35 @@ reactiveAnd( CellPointer1, CellPointer2, {Cell1Val, Cell2Val}, Message ) ->
 			end
 	end.
 	
-%% 
-%% stripName :: KeyedMessage -> UnkeyedMessage
-%% 
 
-stripName( Message ) ->
-	cellMessage:toElement(Message).
-
-
-invert( SelfPointer, CellPointerParent, State, Message ) ->
-	case cellMessage:fromName(Message) of
-		cellPointer:name(CellPointerParent) ->
+invert( SelfPointer, CellPointerParent, State, From, Element ) ->
+	CellName1 = cellPointer:name(CellPointerParent),
+	case cellPointer:name(From) of
+		CellName1 ->
 			% message is {add, {map, {key, value}}}, or remove
 			% create cell for that key if it doesnt exist
 			% key/value then gets used to tag incoming messages
-			NewState = invertStateProcess(State, Message),
+			NewState = invertStateProcess(State, Element),
 			% check if any messages in holding and return them
-			{NewState1, Elements} = invert( CellPointerParent, NewState, Message );
+			{NewState1, Elements} = todo; %invert( CellPointerParent, NewState, Message );
 		CellPointerInformant ->
 			case invertStateGetValue(CellPointerInformant) of
 				{ok, Value} ->
-					MessageElement = cellMessage:setValue(Message),
-					case invertStateGetCellpointer(MessageElement) of
+					ElementValue = cellElement:setValue(Element),
+					case invertStateGetCellpointer(ElementValue) of
 						{ok, CellPointer} ->
 							%send Value to cellpointer using the modifier from message
 							cell:sendElements(
 								CellPointer,
-								Selfpointer, 
-								cellElement:set(cellMessage:modifier(Message), Value),
-							{State, [must return elements for weighting]};
+								SelfPointer, 
+								cellElement:set(cellElement:modifier(Element), Value),
+							{State, [must_return_elements_for_weighting]});
 						error ->
 							%create cell associated with cellMessage:setValue(Message)
 							%add Value to that new cell
 							%store location of cell, return {state, {map, {MessageElement, cell}}}
+							todo
+					end;
 				error ->
 					invertStateHoldMessage(Message)
 			end
@@ -132,16 +136,17 @@ invert( SelfPointer, CellPointerParent, State, Message ) ->
 %% Message should be like {add, {map, {a1, cellpointerb's}}}
 %% 
 
-invertStateProcess(State, Message) ->
-	case cellMessage:modifier(Message) of
+invertStateProcess(State, Element) ->
+	case cellElement:modifier(Element) of
 		add ->
 			CellPointerSetOfAs = cell:makeCell(set),
 			invertStateStoreKeydInput(
-				cellPointer:name(cellMessage:mapValue(Message)),
-				cellMessage:mapKey(Message)
+				cellPointer:name(cellMessage:mapValue(Element)),
+				cellMessage:mapKey(Element)
 			),
-			
+			todo;
 		remove ->
+			todo
 	end.
 	
 invertBaseState() ->
@@ -149,13 +154,15 @@ invertBaseState() ->
 	
 %if b comes from cellname, store tag in cell associated with b
 invertStateStoreKeydInput(CellFromName, Tag) ->
-	.
+	todo.
 	
+invertStateGetValue(_) -> todo.
+invertStateGetCellpointer(_) -> todo.
 
-setDifference(CellPointer1, CellPointer2, State, {done...})
-setDifference(CellPointer1, CellPointer2, State, Message) ->
-	Value = cellElement:value(cellMessage:toElement(Message)),
-	case cellMessage:fromName(Message) of
+
+setDifference(CellPointer1, CellPointer2, State, From, Element) ->
+	Value = cellElement:value(Element),
+	case cellPointer:name(From) of
 		cellPointer:name(CellPointer1) ->
 			case cellElement:modifier(Element) of
 				add ->
